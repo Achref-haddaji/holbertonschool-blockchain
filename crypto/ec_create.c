f1dc811597670bc86224efecbe6b0f585cf362b4
Achref-haddaji <haddaji.achref19@gmail.com>
2023-07-27 16:39:06 +0100

    task1

--- crypto/ec_create.c
+++ crypto/ec_create.c
@@ -1,34 +1,39 @@
 #include <stdlib.h>
 #include "hblk_crypto.h"
 
+/**
+ * ec_create - Create a new EC key pair
+ *
+ * Return: Pointer to the generated EC_KEY structure, or NULL upon failure
+ */
 EC_KEY *ec_create(void)
 {
-    EC_KEY *key = NULL;
-    EC_GROUP *group = NULL;
+	EC_KEY *key = NULL;
+	EC_GROUP *group = NULL;
 
-    /* Création d'une nouvelle structure EC_KEY */
-    key = EC_KEY_new();
-    if (!key)
-        return NULL;
+    /* Create a new EC_KEY structure */
+	key = EC_KEY_new();
+	if (!key)
+		return (NULL);
 
-    /* Sélection de la courbe elliptique secp256k1 */
-    group = EC_GROUP_new_by_curve_name(EC_CURVE);
-    if (!group)
-    {
-        EC_KEY_free(key);
-        return NULL;
-    }
+    /* Select the secp256k1 elliptic curve */
+	group = EC_GROUP_new_by_curve_name(EC_CURVE);
+	if (!group)
+	{
+		EC_KEY_free(key);
+		return (NULL);
+	}
 
-    /* Génération de la clé privée et de la paire de clés */
-    if (EC_KEY_set_group(key, group) != 1 || EC_KEY_generate_key(key) != 1)
-    {
-        EC_KEY_free(key);
-        EC_GROUP_free(group);
-        return NULL;
-    }
+    /* Generate the private key and key pair */
+	if (EC_KEY_set_group(key, group) != 1 || EC_KEY_generate_key(key) != 1)
+	{
+		EC_KEY_free(key);
+		EC_GROUP_free(group);
+		return (NULL);
+	}
 
-    /* Libération de la mémoire utilisée pour le groupe elliptique */
-    EC_GROUP_free(group);
+    /* Free the memory used for the elliptic group */
+	EC_GROUP_free(group);
 
-    return key;
+	return (key);
 }
