ca8122c9914b1f404be8ea4cf205b0dba7e32b10
Achref-haddaji <haddaji.achref19@gmail.com>
2023-07-28 14:37:13 +0100

    task0

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -1,30 +1,19 @@
-#include <hblk_crypto.h>
+#include "hblk_crypto.h"
 
 /**
- * sha256 - Computes the SHA-256 hash of a sequence of bytes
+ * sha256- encrypt a a string using SHA256 algorithm
  *
- * @s: The sequence of bytes to be hashed
- * @len: The number of bytes to hash in s
- * @digest: The array to store the resulting hash
- *
- * Return: Pointer to the digest array, or NULL if digest is NULL
+ * @s: the string
+ * @len: the length of the string
+ * @digest: the buffer for hash
+ * Return: if the buffer digest is null rturn NULL otherwise hash string
  */
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest
-[SHA256_DIGEST_LENGTH])
+
+uint8_t *sha256(int8_t const *s, size_t len,
+		uint8_t digest[SHA256_DIGEST_LENGTH])
 {
 	if (!digest)
 		return (NULL);
 
-	SHA256_CTX sha256_ctx;
-
-	if (!SHA256_Init(&sha256_ctx))
-		return (NULL);
-
-	if (!SHA256_Update(&sha256_ctx, s, len))
-		return (NULL);
-
-	if (!SHA256_Final(digest, &sha256_ctx))
-		return (NULL);
-
-	return (digest);
+	return (SHA256((const unsigned char *)s, len, digest));
 }
