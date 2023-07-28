d6e76649bbcad318391a281a0a2e9f7f7fc9976c
Achref-haddaji <haddaji.achref19@gmail.com>
2023-07-28 14:59:42 +0100

    task2

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -1,34 +1,35 @@
-#include <stdlib.h>
 #include "hblk_crypto.h"
-
 /**
- * ec_to_pub - Extracts the public key from an EC_KEY structure
- *
- * @key: Pointer to the EC_KEY structure to retrieve the public key from
- * @pub: Address at which to store the extracted public key (not compressed)
- *
- * Return: Pointer to pub, or NULL upon failure
+ * ec_to_pub-extract public key from EC_KEY
+ * @key: EC_KEY to extract public key from
+ * @pub: buffer that will containing public key
+ *Return: if successful  return Buffer public key otherwise return NULL
  */
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 {
+	const EC_POINT *pubKey;
+	const EC_GROUP *group;
+
 	if (!key || !pub)
 		return (NULL);
 
-	const EC_POINT *point = EC_KEY_get0_public_key(key);
-
-	if (!point)
+	pubKey = EC_KEY_get0_public_key(key);
+	if (!pubKey)
+	{
+		fprintf(stderr, "EC_KEY_get0_public_key() is failed\n");
 		return (NULL);
-
-	const EC_GROUP *group = EC_KEY_get0_group(key);
-
+	}
+	group = EC_KEY_get0_group(key);
 	if (!group)
+	{
+		fprintf(stderr, "EC_KEY_get0_group() is failed\n");
 		return (NULL);
-
-	size_t len = EC_POINT_point2oct(group, point, POINT_CONVERSION_UNCOMPRESSED,
-	 pub, EC_PUB_LEN, NULL);
-
-	if (len != EC_PUB_LEN)
+	}
+	if (!EC_POINT_point2oct(group, pubKey, POINT_CONVERSION_UNCOMPRESSED,
+									pub, EC_PUB_LEN, NULL))
+	{
+		fprintf(stderr, "EC_POINT_point2oct() is failed\n");
 		return (NULL);
-
+	}
 	return (pub);
 }
