28733b3f337ba58ebe8a2c2f8f5d8671f7888c6a
Achref-haddaji <haddaji.achref19@gmail.com>
2023-07-29 16:23:15 +0100

    task7

--- crypto/ec_verify.c
+++ crypto/ec_verify.c
@@ -4,7 +4,7 @@
 #include "hblk_crypto.h"
 
 /**
- * ec_verify - Verifies the signature of a given set of bytes using a given EC_KEY public key
+ * ec_verify - Verifies the signature
  *
  * @key: Pointer to the EC_KEY structure containing the public key
  * @msg: Pointer to the bytes to verify the signature of
@@ -13,10 +13,11 @@
  *
  * Return: 1 if the signature is valid, 0 otherwise
  */
-int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t const *sig)
+int ec_verify(
+	EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t const *sig)
 {
-    if (!key || !msg || !sig)
-        return 0;
+	if (!key || !msg || !sig)
+	return (0);
 
-    return (ECDSA_verify(0, msg, msglen, sig->sig, sig->len, (EC_KEY *)key) == 1);
+	return (ECDSA_verify(0, msg, msglen, sig->sig, sig->len, (EC_KEY *)key) == 1);
 }
