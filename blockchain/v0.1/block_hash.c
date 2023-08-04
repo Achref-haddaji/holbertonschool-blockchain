1cc58a750f462a210dfbbf4b4b5078cc4148b641
Achref-haddaji <haddaji.achref19@gmail.com>
2023-08-04 17:33:44 +0100

    task4

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -9,12 +9,13 @@
  *
  * Return: Pointer to hash_buf
  */
-uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+uint8_t *block_hash(
+	block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-    if (block == NULL || hash_buf == NULL)
-        return (NULL);
+	if (block == NULL || hash_buf == NULL)
+		return (NULL);
 
-    sha256((int8_t *)&block->info, sizeof(block->info), hash_buf);
+	sha256((int8_t *)&block->info, sizeof(block->info), hash_buf);
 
-    return (hash_buf);
+	return (hash_buf);
 }
